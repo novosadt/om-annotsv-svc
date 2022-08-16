4379ce073b76831a23dc1f0a6cbb3e49ae085e6b
tnov <tnov@bach.cz>
2022-08-16 15:01:46 +0200

    samplot variant generator

--- src/main/java/cz/vsb/genetics/svc/main/BionanoSamplotSvComparator.java
+++ src/main/java/cz/vsb/genetics/svc/main/BionanoSamplotSvComparator.java
@@ -48,6 +48,8 @@ public class BionanoSamplotSvComparator {
                         forks);
             }
 
+
+
         }
         catch (Exception e) {
             System.out.println("Error occurred:");
